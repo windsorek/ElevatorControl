1a5c771fbcde1f1abee7ee8a7c0e1aa78fdef5e7
Krzysztof Kwasniewski <kwasniewski.krzysztoff@wp.pl>
2021-04-22 22:14:24 +0200

    new test data - for easytesting

--- ElevatorControl/ElevatorControl/Data/TestData.cs
+++ ElevatorControl/ElevatorControl/Data/TestData.cs
@@ -13,24 +13,26 @@ namespace ElevatorControl.Data
         public static void AddTestData(ElevatorControlContext elevatorControlContext)
         {
             var rng = new Random();
-            const int totalFloorsMax = 10;
+            const int totalFloorsMax = 30;
+            const int totalFloorsMin = 4;
             const int lowestFloorMin = -10;
             const int lowestFloorMax = 10;
-            elevatorControlContext.Elevators.Add(new Elevator((uint)rng.Next(1, totalFloorsMax), rng.Next(lowestFloorMin, lowestFloorMax)));
-            elevatorControlContext.Elevators.Add(new Elevator((uint)rng.Next(1, totalFloorsMax), rng.Next(lowestFloorMin, lowestFloorMax)));
-            elevatorControlContext.Elevators.Add(new Elevator((uint)rng.Next(1, totalFloorsMax), rng.Next(lowestFloorMin, lowestFloorMax)));
+            elevatorControlContext.Elevators.Add(new Elevator((uint)rng.Next(totalFloorsMin, totalFloorsMax), rng.Next(lowestFloorMin, lowestFloorMax)));
+            elevatorControlContext.Elevators.Add(new Elevator((uint)rng.Next(totalFloorsMin, totalFloorsMax), rng.Next(lowestFloorMin, lowestFloorMax)));
+            elevatorControlContext.Elevators.Add(new Elevator((uint)rng.Next(totalFloorsMin, totalFloorsMax), rng.Next(lowestFloorMin, lowestFloorMax)));
             elevatorControlContext.SaveChanges();
         }
 
         public static void AddTestData(IElevatorService elevatorsService)
         {
             var rng = new Random();
-            const int totalFloorsMax = 10;
+            const int totalFloorsMax = 30;
+            const int totalFloorsMin = 4;
             const int lowestFloorMin = -10;
             const int lowestFloorMax = 10;
-            elevatorsService.AddElevator(new Elevator((uint)rng.Next(1, totalFloorsMax), rng.Next(lowestFloorMin, lowestFloorMax)));
-            elevatorsService.AddElevator(new Elevator((uint)rng.Next(1, totalFloorsMax), rng.Next(lowestFloorMin, lowestFloorMax)));
-            elevatorsService.AddElevator(new Elevator((uint)rng.Next(1, totalFloorsMax), rng.Next(lowestFloorMin, lowestFloorMax)));
+            elevatorsService.AddElevator(new Elevator((uint)rng.Next(totalFloorsMin, totalFloorsMax), rng.Next(lowestFloorMin, lowestFloorMax)));
+            elevatorsService.AddElevator(new Elevator((uint)rng.Next(totalFloorsMin, totalFloorsMax), rng.Next(lowestFloorMin, lowestFloorMax)));
+            elevatorsService.AddElevator(new Elevator((uint)rng.Next(totalFloorsMin, totalFloorsMax), rng.Next(lowestFloorMin, lowestFloorMax)));
             
         }
     }
