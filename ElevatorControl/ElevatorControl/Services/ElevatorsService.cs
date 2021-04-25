578a649f20ffd38dab0aeb403789e38fdf3b4964
Krzysztof Kwasniewski <kwasniewski.krzysztoff@wp.pl>
2021-04-25 22:31:21 +0200

    forgotten unnecessary using

--- ElevatorControl/ElevatorControl/Services/ElevatorsService.cs
+++ ElevatorControl/ElevatorControl/Services/ElevatorsService.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using App.Metrics.Concurrency;
 using ElevatorControl.Interfaces;
 using Microsoft.Extensions.Logging;
 
