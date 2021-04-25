db5dca42b1f6f3233084d71b1e10945cec770a5e
Krzysztof Kwasniewski <kwasniewski.krzysztoff@wp.pl>
2021-04-25 22:09:20 +0200

    corrected "Elevator closing and opening doors speed 2 seconds." for tests

--- ElevatorControl/XUnitTests_ElevatorControl/ElevatorClassTests.cs
+++ ElevatorControl/XUnitTests_ElevatorControl/ElevatorClassTests.cs
@@ -19,13 +19,13 @@ namespace XUnitTests_ElevatorControl
         }
 
         [Theory]
-        [InlineData(-5,false)]
-        [InlineData(-1,false)]
-        [InlineData(0,true)]
-        [InlineData(5,true)]
-        [InlineData(10,true)]
-        [InlineData(15,false)]
-        [InlineData(20,false)]
+        [InlineData(-5, false)]
+        [InlineData(-1, false)]
+        [InlineData(0, true)]
+        [InlineData(5, true)]
+        [InlineData(10, true)]
+        [InlineData(15, false)]
+        [InlineData(20, false)]
         public void ElevatorRequestFloor(int targetFloor, bool expectedResult)
         {
             ///Arrange
@@ -34,7 +34,7 @@ namespace XUnitTests_ElevatorControl
             ///Act
             bool retVal = elevator.CallMe(elevatorRequest);
             ///Assert
-            Assert.Equal(expectedResult,retVal);
+            Assert.Equal(expectedResult, retVal);
         }
 
         [Theory]
@@ -69,7 +69,7 @@ namespace XUnitTests_ElevatorControl
             ///Arrange
             int startingFloor = 0;
             int floorMovingSpeed = 1000;//ms
-            int doorOperatingSpeed = 1000;//ms
+            int doorOperatingSpeed = 2000;//ms
             Elevator elevator = new Elevator(10, startingFloor);
             ElevatorRequest elevatorRequest = new ElevatorRequest(targetFloor);
             ///Act
@@ -80,9 +80,10 @@ namespace XUnitTests_ElevatorControl
             {
                 Thread.Sleep(floorMovingSpeed * (targetFloor - startingFloor) + 2 * doorOperatingSpeed + 1500);
                 Assert.Equal(elevator.CurrentFloor, targetFloor);
-                Assert.True(elevator.DoorState == DoorState.Opened,$"DoorState {elevator.DoorState} but should be opened ");
+                Assert.True(elevator.DoorState == DoorState.Opened, $"DoorState {elevator.DoorState} but should be opened ");
                 Assert.True(elevator.ElevatorState == ElevatorState.Ready, $"Elevator State {elevator.ElevatorState} but should be ready");
-            }else
+            }
+            else
             {
                 Assert.Equal(elevator.CurrentFloor, startingFloor);
             }
