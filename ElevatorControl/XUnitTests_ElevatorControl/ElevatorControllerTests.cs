ab7435fd6fbc3b38cc45c20651d4700be5313210
Krzysztof Kwasniewski <kwasniewski.krzysztoff@wp.pl>
2021-04-25 13:08:22 +0200

    Controller tests added

---
+++ ElevatorControl/XUnitTests_ElevatorControl/ElevatorControllerTests.cs
@@ -0,0 +1,68 @@
+﻿using ElevatorControl.Controllers;
+using ElevatorControl.Interfaces;
+using ElevatorControl.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTests_ElevatorControl
+{
+    public class ElevatorControllerTests
+    {
+        [Fact]
+        public async Task GetElevatorsTest()
+        {
+            // Arrange
+            Elevator elevator = new Elevator(10, 0);
+            List<Elevator> elevators = new List<Elevator>();
+            elevators.Add(elevator);
+            Mock<IElevatorService> mockElevatorService = new Mock<IElevatorService>();
+
+            mockElevatorService.Setup(mock => mock.GetElevator()).ReturnsAsync(elevators);
+
+            ElevatorsController controller = new ElevatorsController(mockElevatorService.Object);
+
+            // Act
+            var result = await controller.GetElevator();
+
+            // Assert
+            Assert.IsType<ActionResult<IEnumerable<Elevator>>>(result);
+            Assert.Equal(result.Value.ToList().Count(), elevators.Count());
+
+        }
+
+        [Theory]
+        [InlineData(0, true)]
+        [InlineData(1, false)]
+        public async Task GetElevatorTest(int id, bool success)
+        {
+            // Arrange
+            Elevator elevator = new Elevator(10, 0);
+            Mock<IElevatorService> mockElevatorService = new Mock<IElevatorService>();
+
+            mockElevatorService.Setup(mock => mock.GetElevator(id)).ReturnsAsync(elevator);
+
+            ElevatorsController controller = new ElevatorsController(mockElevatorService.Object);
+
+            // Act
+            ActionResult<Elevator> result = await controller.GetElevator(0);
+
+            // Assert
+            Assert.IsType<ActionResult<Elevator>>(result);
+            if (success)
+            {
+                Assert.Equal(result.Value, elevator);
+            }
+            else
+            {
+                Assert.NotEqual(result.Value, elevator);
+                Assert.Null(result.Value);
+            }
+        }
+    }
+}
